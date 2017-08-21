36b85848d8a6a6b9202ced4d3b7999eb15483c37
10p26r <31011752+10p26r@users.noreply.github.com>
2017-08-22 02:15:37 +0700

    Create Proxy.sh

---
+++ Proxy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# go to root
+cd
+# Install Command
+apt-get -y install sudo
+
+# Install Squid
+apt-get -y install squid3
+cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/zero9911/pritunl/master/conf/squid.conf" 
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
+sed -i s/xxxxxxxxx/$MYIP/g /etc/squid3/squid.conf;
+service squid3 restart
+clear
+echo ""
+echo "Success Install Squid3 Proxy"
+echo ""
