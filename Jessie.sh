49b660a4c16f49ef775e2e332fc2254760456c74
10p26r <31011752+10p26r@users.noreply.github.com>
2017-08-22 23:36:10 +0700

    Update Jessie.sh

--- Jessie.sh
+++ Jessie.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Go to Root
+# go to root
 cd
 
 # Install Command
@@ -8,16 +8,17 @@ apt-get -y install ufw
 apt-get -y install sudo
 
 # Install Pritunl
-echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" > /etc/apt/sources.list.d/mongodb-org-3.4.list
+echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" > /etc/apt/sources.list.d/mongodb-org-3.2.list
 echo "deb http://repo.pritunl.com/stable/apt jessie main" > /etc/apt/sources.list.d/pritunl.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 0C49F3730359A14518585931BC711F9BA15703C6
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv EA312927
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv CF8E292A
 apt-get -y update
-apt-get --assume-yes install pritunl mongodb-org
+apt-get -y upgrade
+apt-get -y install pritunl mongodb-org
 systemctl start mongod pritunl
 systemctl enable mongod pritunl
 
-# Install Squid Proxy
+# Install Squid
 apt-get -y install squid3
 cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
 wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/zero9911/pritunl/master/conf/squid.conf" 
@@ -30,8 +31,41 @@ sudo ufw allow 22,80,81,222,443,8080,9700,60000/tcp
 sudo ufw allow 22,80,81,222,443,8080,9700,60000/udp
 sudo yes | ufw enable
 
-# Change to Time Zone
+# Change to Time GMT+8
 ln -fs /usr/share/zoneinfo/Asia/Bangkok /etc/localtime
+
+# Install Web Server
+apt-get -y install nginx php5-fpm php5-cli
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/zero9911/pritunl/master/conf/nginx.conf"
+mkdir -p /home/vps/public_html
+echo "<pre>Setup by MKSSHVPN </pre>" > /home/vps/public_html/index.html
+echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/zero9911/pritunl/master/conf/vps.conf"
+sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
+service php5-fpm restart
+service nginx restart
+
+# Install Vnstat
+apt-get -y install vnstat
+vnstat -u -i eth0
+sudo chown -R vnstat:vnstat /var/lib/vnstat
+service vnstat restart
+
+# Install Vnstat GUI
+cd /home/vps/public_html/
+wget http://www.sqweek.com/sqweek/files/vnstat_php_frontend-1.5.1.tar.gz
+tar xf vnstat_php_frontend-1.5.1.tar.gz
+rm vnstat_php_frontend-1.5.1.tar.gz
+mv vnstat_php_frontend-1.5.1 vnstat
+cd vnstat
+sed -i "s/\$iface_list = array('eth0', 'sixxs');/\$iface_list = array('eth0');/g" config.php
+sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+sed -i 's/Internal/Internet/g' config.php
+sed -i '/SixXS IPv6/d' config.php
+cd
 
 # About
 clear
@@ -43,5 +77,4 @@ echo ""
 echo "Copy URL and Pless on Browser : https://$MYIP"
 echo "Copy Code and Pless to Pritunl"
 pritunl setup-key
-echo ""
 echo ""
