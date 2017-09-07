e72fe9b7afdf05302c54f85b916398011a8d3e4b
10p26r <31011752+10p26r@users.noreply.github.com>
2017-09-07 22:06:06 +0700

    Create squid3.sh

---
+++ squid3.sh
@@ -0,0 +1,6 @@
+apt-get -y install squid3
+cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/zero9911/pritunl/master/conf/squid.conf" 
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
+sed -i s/xxxxxxxxx/$MYIP/g /etc/squid3/squid.conf;
+service squid3 restart
