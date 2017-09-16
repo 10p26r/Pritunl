b64c3e18103be4184e8ba85c34af757d67bb9e42
10p26r <31011752+10p26r@users.noreply.github.com>
2017-09-17 06:55:40 +0700

    Delete 1.sh

--- 1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
-wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/10p26r/Pritunl/master/2.conf"
-MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
-sed -i s/xxxxxxxxx/$MYIP/g /etc/squid3/squid.conf;
