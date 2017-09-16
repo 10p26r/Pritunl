d022f2cd036e4536035e067b12448d0a075f29eb
10p26r <31011752+10p26r@users.noreply.github.com>
2017-09-17 01:42:23 +0700

    Update Squid+Squid3.sh

--- Squid+Squid3.sh
+++ Squid+Squid3.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-apt-get -y install squid
-cp /etc/squid/squid.conf /etc/squid/squid.conf.orig
-wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/10p26r/Pritunl/master/squid3.conf"
-MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
-sed -i s/xxxxxxxxx/$MYIP/g /etc/squid/squid.conf;
-service squid restart
-
 apt-get -y install squid3
 cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
 wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/10p26r/Pritunl/master/squid3.conf"
