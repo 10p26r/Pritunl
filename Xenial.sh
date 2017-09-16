d6ab3acecb9721f40dcc9306d3cb5e05fc04faa9
10p26r <31011752+10p26r@users.noreply.github.com>
2017-09-17 05:56:27 +0700

    Update Xenial.sh

--- Xenial.sh
+++ Xenial.sh
@@ -15,25 +15,6 @@ sudo apt-get update
 sudo apt-get --assume-yes install pritunl mongodb-org
 sudo systemctl start pritunl mongod
 sudo systemctl enable pritunl mongod
-
-# Install Squid Proxy
-apt-get -y install squid3
-cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
-wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/10p26r/Pritunl/master/Squid3.conf"
-MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
-sed -i s/xxxxxxxxx/$MYIP/g /etc/squid3/squid.conf;
-service squid3 restart
 echo ""
-echo "Please wait restart Squid...."
-sudo service squid restart
-cd
-
-# End
-echo "Pritunl and Squid Proxy ..Install Complete"
-echo "Source by Mnm Ami"
-echo ""
-echo "Squid Proxy : $MYIP Port 80, 8000, 8080"
-echo "Pritunl : https://$MYIP"
-echo "Copy Key and Pless to Pritunl"
-pritunl setup-key
+echo "Install Pritunl Complete..."
 echo ""
