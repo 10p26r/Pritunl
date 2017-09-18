3c22a30a56c4cb6a3913e2d69a1b45f5e0bda2a9
10p26r <31011752+10p26r@users.noreply.github.com>
2017-09-18 21:57:10 +0700

    Update Install.sh

--- Extra/Install.sh
+++ Extra/Install.sh
@@ -2,6 +2,7 @@
 
 myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
 myint=`ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}'`;
+cd
 
 if [ $USER != 'root' ]; then
 	echo "Sorry, for run the script please using root user"
@@ -12,8 +13,22 @@ if [[ ! -e /dev/net/tun ]]; then
 	exit
 fi
 
-ln -fs /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime;
+ln -fs /usr/share/zoneinfo/Asia/Thailand /etc/localtime;
 
+echo ipv4 >> /etc/modules
+echo ipv6 >> /etc/modules
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+sed -i 's/#net.ipv6.conf.all.forwarding=1/net.ipv6.conf.all.forwarding=1/g' /etc/sysctl.conf
+sysctl -p
+cd
+
+# Fail2ban & Exim & Protection
+apt-get -y install fail2ban sysv-rc-conf dnsutils dsniff zip unzip;
+wget https://github.com/jgmdev/ddos-deflate/archive/master.zip;unzip master.zip;
+cd ddos-deflate-master && ./install.sh
+service exim4 stop;sysv-rc-conf exim4 off;
+cd
 
 # SQUID
 apt-get -y install squid
@@ -36,12 +51,14 @@ chmod +x  /usr/local/bin/status
 chmod +x  /usr/local/bin/monssh
 cd
 
+# Dropbear
 apt-get -y install dropbear
 wget -O /etc/default/dropbear "https://raw.githubusercontent.com/10p26r/Pritunl/master/Extra/dropbear"
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
 cd
 
+# OpenVPN
 apt-get -y install openvpn
 cd /etc/openvpn/
 wget https://raw.githubusercontent.com/zero9911/a/master/script/openvpn/openvpn.tar;tar xf openvpn.tar;rm openvpn.tar
@@ -56,15 +73,14 @@ cd;wget https://raw.githubusercontent.com/zero9911/a/master/script/openvpn/cronj
 tar xf cronjob.tar;mv uptime.php /home/vps/public_html/
 mv usertol userssh uservpn /usr/bin/;mv cronvpn cronssh /etc/cron.d/
 chmod +x /usr/bin/usertol;chmod +x /usr/bin/userssh;chmod +x /usr/bin/uservpn;
-useradd -m -g users -s /bin/bash dragon
-echo "dragon:369" | chpasswd
 echo "UPDATE AND INSTALL COMPLETE COMPLETE 99% BE PATIENT"
 rm $0;rm *.txt;rm *.tar;rm *.deb;rm *.asc;rm *.zip;rm ddos*;
-clear
+cd
 
 # restart service
 service ssh restart
 service dropbear restart
+service openvpn restart
 service squid restart
 
 clear
