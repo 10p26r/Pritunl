083c5e5c8200a4f359ac49fd1236d6b279fb1640
10p26r <31011752+10p26r@users.noreply.github.com>
2017-09-18 23:59:44 +0700

    Update Install.sh

--- Extra/Install.sh
+++ Extra/Install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
 myint=`ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}'`;
 
@@ -21,7 +20,7 @@ clear
 echo "START AUTOSCRIPT"
 clear
 echo "SET TIMEZONE KUALA LUMPUT GMT +8"
-ln -fs /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime;
+ln -fs /usr/share/zoneinfo/Asia/Thailand /etc/localtime;
 clear
 echo "
 ENABLE IPV4 AND IPV6
@@ -49,15 +48,6 @@ echo "
 UPDATE AND UPGRADE PROCESS 
 PLEASE WAIT TAKE TIME 1-5 MINUTE
 "
-sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
-wget -qO - http://www.webmin.com/jcameron-key.asc | apt-key add -
-apt-get update;
-apt-get -y upgrade;
-apt-get -y install wget curl;
-echo "
-INSTALLER PROCESS PLEASE WAIT
-TAKE TIME 5-10 MINUTE
-"
 
 #install menu
 wget https://raw.githubusercontent.com/zero9911/a/master/script/menu
@@ -79,15 +69,7 @@ cd
 wget https://raw.githubusercontent.com/zero9911/a/master/script/motd
 mv ./motd /etc/motd
 
-# fail2ban & exim & protection
-apt-get -y install fail2ban sysv-rc-conf dnsutils dsniff zip unzip;
-wget https://github.com/jgmdev/ddos-deflate/archive/master.zip;unzip master.zip;
-cd ddos-deflate-master && ./install.sh
-service exim4 stop;sysv-rc-conf exim4 off;
 
-# webmin
-apt-get -y install webmin
-sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 
 # ssh
 sed -i 's/#Banner/Banner/g' /etc/ssh/sshd_config
@@ -102,24 +84,13 @@ echo "/usr/sbin/nologin" >> /etc/shells
 cd
 
 # squid3
-apt-get -y install squid3
-wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/zero9911/a/master/script/squid.conf"
-sed -i "s/ipserver/$myip/g" /etc/squid3/squid.conf
-cd
-
-# nginx
-apt-get -y install nginx php5-fpm php5-cli libexpat1-dev libxml-parser-perl
-rm /etc/nginx/sites-enabled/default
-rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/zero9911/a/master/script/nginx.conf"
-mkdir -p /home/vps/public_html
-echo "<pre>Setup by Dragon96 | telegram @ranger_9699 | whatsapp +60162327524</pre>" > /home/vps/public_html/index.php
-echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/zero9911/a/master/script/vps1.conf"
-sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
+apt-get -y install squid
+wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/zero9911/a/master/script/squid.conf"
+sed -i "s/ipserver/$myip/g" /etc/squid/squid.conf
 cd
 
 # openvpn
+mkdir -p /home/vps/public_html
 apt-get -y install openvpn
 cd /etc/openvpn/
 wget https://raw.githubusercontent.com/zero9911/a/master/script/openvpn/openvpn.tar;tar xf openvpn.tar;rm openvpn.tar
@@ -134,21 +105,14 @@ cd;wget https://raw.githubusercontent.com/zero9911/a/master/script/openvpn/cronj
 tar xf cronjob.tar;mv uptime.php /home/vps/public_html/
 mv usertol userssh uservpn /usr/bin/;mv cronvpn cronssh /etc/cron.d/
 chmod +x /usr/bin/usertol;chmod +x /usr/bin/userssh;chmod +x /usr/bin/uservpn;
-useradd -m -g users -s /bin/bash dragon
-echo "dragon:369" | chpasswd
 echo "UPDATE AND INSTALL COMPLETE COMPLETE 99% BE PATIENT"
-rm $0;rm *.txt;rm *.tar;rm *.deb;rm *.asc;rm *.zip;rm ddos*;
 cd
 
 # restart service
 service ssh restart
 service openvpn restart
 service dropbear restart
-service nginx restart
-service php5-fpm restart
-service webmin restart
-service squid3 restart
-service fail2ban restart
+service squid restart
 
 
 echo "
@@ -156,7 +120,5 @@ echo "
 Install Complete.......
 "
 cat /dev/null > ~/.bash_history && history -c
-rm *.txt
-rm *.sh
 exit
 fi
