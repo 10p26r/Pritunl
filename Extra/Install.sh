b7c7e38b477279f5fc006a00af567151734abbfb
10p26r <31011752+10p26r@users.noreply.github.com>
2017-09-18 15:43:22 +0700

    Update Install.sh

--- Extra/Install.sh
+++ Extra/Install.sh
@@ -19,12 +19,15 @@ cd
 wget https://raw.githubusercontent.com/10p26r/Pritunl/master/Extra/menu
 wget https://raw.githubusercontent.com/10p26r/Pritunl/master/Extra/user-list
 wget https://raw.githubusercontent.com/10p26r/Pritunl/master/Extra/status
+wget https://raw.githubusercontent.com/10p26r/Pritunl/master/Extra/monssh
 mv menu /usr/local/bin/
 mv user-list /usr/local/bin/
 mv status /usr/local/bin/
+mv monssh /usr/local/bin/
 chmod +x  /usr/local/bin/menu
 chmod +x  /usr/local/bin/user-list
 chmod +x  /usr/local/bin/status
+chmod +x  /usr/local/bin/monssh
 cd
 
 # restart service
