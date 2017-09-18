c95e6cae2a0cbd42b659487b7c209a80159e85fb
10p26r <31011752+10p26r@users.noreply.github.com>
2017-09-18 15:19:19 +0700

    Update Install.sh

--- Extra/Install.sh
+++ Extra/Install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
-myint=`ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}'`;
-
 if [ $USER != 'root' ]; then
 	echo "Sorry, for run the script please using root user"
 	exit
@@ -12,19 +9,11 @@ if [[ ! -e /dev/net/tun ]]; then
 	exit
 fi
 
-ln -fs /usr/share/zoneinfo/Asia/Thailand /etc/localtime;
-echo ipv4 >> /etc/modules
-echo ipv6 >> /etc/modules
-sysctl -w net.ipv4.ip_forward=1
-sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-sed -i 's/#net.ipv6.conf.all.forwarding=1/net.ipv6.conf.all.forwarding=1/g' /etc/sysctl.conf
-sysctl -p
-apt-get -y --purge remove samba*;
-apt-get -y --purge remove apache2*;
-apt-get -y --purge remove sendmail*;
-apt-get -y --purge remove postfix*;
-apt-get -y --purge remove bind*;
-clear
+# SQUID
+apt-get -y install squid
+wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/zero9911/a/master/script/squid.conf"
+sed -i "s/ipserver/$myip/g" /etc/squid/squid.conf
+cd
 
 # MENU
 wget https://raw.githubusercontent.com/10p26r/Pritunl/master/Extra/menu
@@ -41,44 +30,8 @@ chmod +x  /usr/local/bin/monssh
 chmod +x  /usr/local/bin/status
 cd
 
-# DROPBEAR
-apt-get -y install dropbear
-wget -O /etc/default/dropbear "https://raw.githubusercontent.com/zero9911/a/master/script/dropbear"
-echo "/bin/false" >> /etc/shells
-echo "/usr/sbin/nologin" >> /etc/shells
-cd
-
-# SQUID3
-apt-get -y install squid
-wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/zero9911/a/master/script/squid.conf"
-sed -i "s/ipserver/$myip/g" /etc/squid/squid.conf
-cd
-
-
-# OPENVPN
-apt-get -y install openvpn
-cd /etc/openvpn/
-wget https://raw.githubusercontent.com/zero9911/a/master/script/openvpn/openvpn.tar;tar xf openvpn.tar;rm openvpn.tar
-wget -O /etc/iptables.up.rules "https://raw.githubusercontent.com/zero9911/a/master/script/openvpn/iptables.up.rules"
-sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
-sed -i "s/ipserver/$myip/g" /etc/iptables.up.rules
-iptables-restore < /etc/iptables.up.rules
-# etc
-wget -O /home/vps/public_html/client.ovpn "https://raw.githubusercontent.com/zero9911/a/master/script/openvpn/client.ovpn"
-sed -i "s/ipserver/$myip/g" /home/vps/public_html/client.ovpn
-cd;wget https://raw.githubusercontent.com/zero9911/a/master/script/openvpn/cronjob.tar
-tar xf cronjob.tar;mv uptime.php /home/vps/public_html/
-mv usertol userssh uservpn /usr/bin/;mv cronvpn cronssh /etc/cron.d/
-chmod +x /usr/bin/usertol;chmod +x /usr/bin/userssh;chmod +x /usr/bin/uservpn;
-useradd -m -g users -s /bin/bash dragon
-echo "dragon:369" | chpasswd
-echo "UPDATE AND INSTALL COMPLETE COMPLETE 99% BE PATIENT"
-rm $0;rm *.txt;rm *.tar;rm *.deb;rm *.asc;rm *.zip;rm ddos*;
-
 # restart service
 service ssh restart
-service openvpn restart
-service dropbear restart
 service squid restart
 
 clear
