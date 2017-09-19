9901c743b007cc9e59aeec9b1daa658506e1ab2a
10p26r <31011752+10p26r@users.noreply.github.com>
2017-09-19 07:36:27 +0700

    Update Jessie.sh

--- Jessie.sh
+++ Jessie.sh
@@ -25,6 +25,14 @@ sudo yes | ufw enable
 # Change to Time GMT+8
 ln -fs /usr/share/zoneinfo/Asia/Bangkok /etc/localtime
 
+# Squid3
+apt-get -y install squid3
+cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/10p26r/Pritunl/master/squid3.conf"
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
+sed -i s/xxxxxxxxx/$MYIP/g /etc/squid3/squid.conf;
+service squid3 restart
+
 # About
 clear
 echo ""
